ab419314395d199f94fee50f7a87d19440a5f583
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-03-01 21:23:18 +0800

    Update generate script

--- src/generate.js
+++ src/generate.js
@@ -1,11 +1,18 @@
 const ObjectsToCsv = require('objects-to-csv');
+const { uuid } = require('uuidv4');
 
 const division = ['DG', 'PHG', 'PRG', 'BG'];
 var initialItems = []
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 5000; i++) {
+  let id = uuid()
+  let date = new Date()
+  let dateTxt = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
   initialItems = [...initialItems, {
-    item: `${division[Math.floor(Math.random() * division.length)]} This is sample text`,
-    time: Math.floor((Math.random() * 30) + 2)
+    id: id,
+    date: dateTxt,
+    job: `${division[Math.floor(Math.random() * division.length)]} ${id}`,
+    time: Math.floor((Math.random() * 30) + 2),
+    remarks: "This is a sample text" 
   }]
 }
 
