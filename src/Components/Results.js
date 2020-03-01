07f012f44e8dbeeeabff12dddce2b1ca91dc99cd
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-03-02 04:22:27 +0800

    Format results

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Pie from './Pie';
+import numeral from 'numeral';
 
 export default ({ results, items }) => {
   const total = Object.values(results).reduce((total,item)=>total+item,0);
@@ -15,29 +16,29 @@ export default ({ results, items }) => {
         <tbody>
           <tr>
             <td>Design Group</td>
-            <td style={{ textAlign: 'right' }}>{results.dg} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.dg).format('0,0')} hours</td>
           </tr>
           <tr>
             <td>Photography Group</td>
-            <td style={{ textAlign: 'right' }}>{results.pt} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.pt).format('0,0')} hours</td>
           </tr>
           <tr>
             <td>Production Group</td>
-            <td style={{ textAlign: 'right' }}>{results.pp} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.pp).format('0,0')} hours</td>
           </tr>
           <tr>
             <td>Binding Group</td>
-            <td style={{ textAlign: 'right' }}>{results.bg} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.bg).format('0,0')} hours</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td><strong>Total hours</strong></td>
-            <td style={{ textAlign: 'right' }}><strong>{total} work hours</strong></td>
+            <td style={{ textAlign: 'right' }}><strong>{numeral(total).format('0,0')} work hours</strong></td>
           </tr>
           <tr>
             <td><strong>Total records</strong></td>
-            <td style={{ textAlign: 'right' }}><strong>{items}</strong></td>
+            <td style={{ textAlign: 'right' }}><strong>{numeral(items).format('0,0')}</strong></td>
           </tr>
         </tfoot>
       </Table>
