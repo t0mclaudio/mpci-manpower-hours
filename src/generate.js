8e8948a0485faaed10318bf105494952dc1f06b1
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-03-02 04:09:13 +0800

    Update parameters

--- src/generate.js
+++ src/generate.js
@@ -3,7 +3,7 @@ const { uuid } = require('uuidv4');
 
 const division = ['DG', 'PHG', 'PRG', 'BG'];
 var initialItems = []
-for (let i = 1; i <= 50000; i++) {
+for (let i = 1; i <= 100; i++) {
   let id = uuid()
   let date = new Date()
   let dateTxt = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
@@ -11,7 +11,7 @@ for (let i = 1; i <= 50000; i++) {
     id: id,
     date: dateTxt,
     job: `${division[Math.floor(Math.random() * division.length)]} ${id}`,
-    time: Math.floor((Math.random() * 30) + 2),
+    time: Math.floor((Math.random() * 10) * 2),
     remarks: "This is a sample text" 
   }]
 }
