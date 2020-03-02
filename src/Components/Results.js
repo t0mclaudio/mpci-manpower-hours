6ae4e93747717ce4c7c9182692946316c2e870d9
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-03-02 16:36:52 +0800

    Add average, percentage fields

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -4,7 +4,7 @@ import Pie from './Pie';
 import numeral from 'numeral';
 
 export default ({ results, items }) => {
-  const total = Object.values(results).reduce((total,item)=>total+item,0);
+  const total = Object.values(results.hours).reduce((total,item)=>total+item,0);
   const colors = {
     DG:'#FF6384',
     PHG:'#36A2EB',
@@ -16,35 +16,49 @@ export default ({ results, items }) => {
       <Table>
         <thead>
           <tr>
-            <th colSpan={2}>Results</th>
+            <th colSpan={5}>Results</th>
+          </tr>
+          <tr>
+            <th>Department</th>
+            <th>Average</th>
+            <th>Total</th>
+            <th>%</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td><Swatch color={colors.DG}/>  Design Group</td>
-            <td style={{ textAlign: 'right' }}>{numeral(results.dg).format('0,0')} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(Math.floor(results.hours.DG/results.jobs.DG.length)).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.DG).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.DG/total).format('0%')}</td>
           </tr>
           <tr>
             <td><Swatch color={colors.PHG} />  Photography Group</td>
-            <td style={{ textAlign: 'right' }}>{numeral(results.pt).format('0,0')} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(Math.floor(results.hours.PHG/results.jobs.PHG.length)).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.PHG).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.PHG/total).format('0%')}</td>
           </tr>
           <tr>
             <td><Swatch color={colors.PRG} />  Production Group</td>
-            <td style={{ textAlign: 'right' }}>{numeral(results.pp).format('0,0')} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(Math.floor(results.hours.PRG/results.jobs.PRG.length)).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.PRG).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.PRG/total).format('0%')}</td>
           </tr>
           <tr>
             <td><Swatch color={colors.BG} />  Binding Group</td>
-            <td style={{ textAlign: 'right' }}>{numeral(results.bg).format('0,0')} hours</td>
+            <td style={{ textAlign: 'right' }}>{numeral(Math.floor(results.hours.BG/results.jobs.BG.length)).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.BG).format('0,0')} hrs</td>
+            <td style={{ textAlign: 'right' }}>{numeral(results.hours.BG/total).format('0%')}</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td><strong>Total hours</strong></td>
-            <td style={{ textAlign: 'right' }}><strong>{numeral(total).format('0,0')} work hours</strong></td>
+            <td colSpan={3} style={{ textAlign: 'right' }}><strong>{numeral(total).format('0,0')} work hours</strong></td>
           </tr>
           <tr>
             <td><strong>Total records</strong></td>
-            <td style={{ textAlign: 'right' }}><strong>{numeral(items).format('0,0')}</strong></td>
+            <td colSpan={3} style={{ textAlign: 'right' }}><strong>{numeral(items).format('0,0')}</strong></td>
           </tr>
         </tfoot>
       </Table>
